9d1c927ba0dfe99064c4b1eff8a08a54c7c26f60
Lerra <valeria.strygina.home@gmail.com>
2022-10-24 16:45:29 +0300

    Ньютон так себе

--- lagrange.py
+++ lagrange.py
@@ -1,18 +1,23 @@
 import numpy as np
 
-
+# функция в 17 варианте
 def funct(x):
     func17 = 2 * np.exp(np.cbrt(x / 3) * np.cos(4 * x))
     return func17
 
+def x_data(a,b,n):
+    x = np.zeros(n)
+    for i in range(n):
+        x[i] = a + i * ((b - a) / 4)
+    return x
+
+# интерполяционный полином Лагранжа
 
 def lagrange(n, point, a, b):
     value = 0
-    x = np.zeros(n)
+    x = x_data(a,b,n)
     l = np.zeros(n)
 
-    for i in range(n):
-        x[i] = a + i * ((b - a) / 4)
     for i in range(n):
         l[i] = 1
         for j in range(n):
@@ -20,6 +25,33 @@ def lagrange(n, point, a, b):
                 l[i] *= (point - x[j]) / (x[i] - x[j])
         value += funct(x[i]) * l[i]
     return value
+
+
+# Интерполяционный полином в форме Ньютона
+# Разностные отношения
+
+def div_diff(a, b, n):
+    x = np.zeros(n)
+    y = np.zeros(n)
+    for i in range(n):
+        x[i] = a + i * ((b - a) / 4)
+        y[i] = funct(x[i])
+    coef = np.zeros([n,n])
+    coef[:, 0] = y
+    for j in range(1, n):
+        for i in range(n - j):
+            coef[i][j] = \
+                (coef[i + 1][j - 1] - coef[i][j - 1]) / (x[i + j] - x[i])
+    return coef
+
+def newton_poly(x, n, a, b):
+    x_points = x_data(a,b,n)
+    n = len(x_points) - 1
+    coef = div_diff(a,b,n)[0, :]
+    p = coef[n-1]
+    for k in range(n):
+        p = coef[n-k-1] + (x -x_points[n-k])*p
+    return p
 
 
 def mape(actual, pred):
--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 # 17 вариант
 
 import numpy as np
-import lagrange as l
+import lagrange as p
 
 
 # основная часть программы
+
+a = 0
+b = 2 * np.pi / 3
+x = 1
 
 print("Полином Лагранжа вычисляется по следующей формуле: ")
 print("sum[n,k=1]( f(x_k) * l_k(x)), где l_x(x) = mult[n,j=1]( (x-x_j) / (x_k-x_j) )")
@@ -18,8 +22,10 @@ elif select == 2:
     x = 0.18*np.pi
 elif select == 3:
     x = 0.42*np.pi
-lagrangeres = l.lagrange(5, x, 0, (2*np.pi)/3)
+lagrangeres = p.lagrange(5, x, a, b)
 calculated = 2 * np.exp(np.cbrt(x / 3) * np.cos(4 * x))
+newton_form = p.newton_poly(x, 5, a, b)
 print("Результат интерполирования: ", lagrangeres)
+print("В форме Ньютона: ", newton_form)
 print("Результат вычисления: ", calculated)
-print("Погрешность: ", l.mape(calculated,lagrangeres))
+print("Погрешность: ", p.mape(calculated, lagrangeres))
