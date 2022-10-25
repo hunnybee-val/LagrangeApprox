c73a5a55562a41def5d5128f8bb86261b971b04e
Lerra <valeria.strygina.home@gmail.com>
2022-10-25 21:59:16 +0300

    alright no need to give up!! it's working!!!

--- interface.py
+++ interface.py
@@ -4,8 +4,10 @@ from tkinter import *
 import lagrange as p
 
 def valueLagrange():
-    x = var.get()
-    value = p.lagrange(5, x, 0, 2 * np.pi / 3)
+    a = 0
+    b = 2 * np.pi / 3
+    x = var.get()*np.pi
+    value = p.lagrange(5, x, a, b)
     lbl_result["text"] = value
 
 root = tk.Tk()
@@ -19,12 +21,12 @@ def sel():
    selection = "Теперь нажмите кнопку, чтобы интерполировать"
    label.config(text = selection)
 
-var = IntVar()
-R1 = Radiobutton(root, text="0.095п", variable=var, value=0.095*np.pi, command=sel)
+var = DoubleVar()
+R1 = Radiobutton(root, text="0.095п", variable=var, value=0.095, command=sel)
 R1.pack()
-R2 = Radiobutton(root, text="0.18п", variable=var, value=0.18*np.pi, command=sel)
+R2 = Radiobutton(root, text="0.18п", variable=var, value=0.18, command=sel)
 R2.pack()
-R3 = Radiobutton(root, text="0.42п", variable=var, value=0.42*np.pi, command=sel)
+R3 = Radiobutton(root, text="0.42п", variable=var, value=0.42, command=sel)
 R3.pack()
 
 
